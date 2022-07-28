4cf11c537290067b6816d7b1193348e336fdbe47
William Zhu <Liam.Zhu@protonmail.com>
2022-07-28 00:33:14 -0400

    Modified style slightly.

--- app/Dicewarist.hs
+++ app/Dicewarist.hs
@@ -20,7 +20,8 @@ import GHC.Plugins ( HasCallStack )
 import GHC.IO ( unsafePerformIO )
 import GHC.Settings.Utils ( maybeRead )
 import Control.Monad.Trans.Reader (ReaderT (runReaderT), asks)
-import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans (lift)
+import Data.Functor ((<&>))
 
 type NumberOfDice = Int
 type InputFile = Text
@@ -40,15 +41,21 @@ data InvalidMessage a =
 -- names using InvalidMessage to pass data to functions, heavy in Texts
 
 diceMessage :: InvalidMessage Text
-diceMessage = IM "Dice Selection Error" "You did not input a valid entry. Choose up to 20, or \
+diceMessage = 
+    
+    IM "Dice Selection Error" "You did not input a valid entry. Choose up to 20, or \
     \press cancel to abort." "Aborted on dice selection." numberOfDicePrompt
 
 sourceMessage :: InvalidMessage [Text]
-sourceMessage = IM "Error on Selecting Source File" "You did not select a file. Try again, or \
+sourceMessage = 
+    
+    IM "Error on Selecting Source File" "You did not select a file. Try again, or \
     \hit cancel to abort." "Aborted on source file selection." fileSelectorPrompt
 
 outputPathMessage :: InvalidMessage Text
-outputPathMessage = IM "Error on Selecting Output File" "You did not select or create a file. Try \
+outputPathMessage = 
+    
+    IM "Error on Selecting Output File" "You did not select or create a file. Try \
     \again, or hit cancel to abort." "Aborted on output file selection." outputFilePrompt
 
 -- Common or reusable functions.
@@ -67,16 +74,19 @@ errorMessageBox message =
 -- Functions closely tied up with message prompts, mainly the prompts and
 -- some basic error-handling code.
 
+
+
+removeCarriageReturns ::  Text -> Text 
+removeCarriageReturns = filter ( \u -> u /= '\n' && u /= '\r' )
+
 numberOfDicePrompt :: IO ( Maybe Text )
 numberOfDicePrompt = 
 
-    removeCarriageReturn $ inputBox "Number of Dice?" "How many dice would you like in \
+    ((removeCarriageReturns <$>) <$>) $ inputBox "Number of Dice?" "How many dice would you like in \
     \your diceware annotations? Choose at least 1, up to 20." $ Just "Input your number here."
 
-    where
+  where
     
-    removeCarriageReturn :: IO ( Maybe Text ) -> IO ( Maybe Text )
-    removeCarriageReturn = ( ( filter ( \u -> u /= '\n' && u /= '\r' ) <$> ) <$> )
 
 fileSelectorPrompt :: IO ( Maybe [ Text ] )
 fileSelectorPrompt = openFileDialog "Select a File to Read" "" ["*.txt"] "" False
@@ -85,56 +95,65 @@ outputFilePrompt :: IO ( Maybe Text )
 outputFilePrompt = saveFileDialog "Select or Create a File to Output to" "" ["*.txt"] ""
 
 invalidMessage :: InvalidMessage a -> ( Maybe a -> IO b ) -> IO b
-invalidMessage IM { .. } callback = 
+invalidMessage IM { .. } callback =
 
-    messageBox messageTitle messageContent Info OK >> responseFunction >>=
-    pure . pure . fromMaybe (errorMessageBox errorMessage) >>= callback
+    messageBox messageTitle messageContent Info OK >>
+    responseFunction >>= callback
+    <$> pure . fromMaybe (errorMessageBox errorMessage)
 
 -- Main program.
 
 dicewarist :: IO ()
 dicewarist = runReaderT process =<< inputs
 
-    where
+inputs :: IO Stores
+inputs = ((,,) <$> getNumberOfDice <*> getSourceFile <*> getOutputPath)
 
-    inputs :: IO Stores
-    inputs = ((,,) <$> getNumberOfDice <*> getSourceFile <*> getOutputPath)
+  where
 
     getNumberOfDice :: IO NumberOfDice
     getNumberOfDice = numberOfDicePrompt >>= sanitizeNumberOfDice
+         
+      where 
 
-    sanitizeNumberOfDice :: Maybe Text -> IO NumberOfDice
-    sanitizeNumberOfDice (Just text) 
-        | Just diceNumber <- maybeRead (unpack text) :: Maybe Int,
-          diceNumber <= 20 && diceNumber > 0 = pure diceNumber
-    sanitizeNumberOfDice _ = invalidMessage diceMessage sanitizeNumberOfDice
+        sanitizeNumberOfDice :: Maybe Text -> IO NumberOfDice
+        sanitizeNumberOfDice (Just text) 
+
+            | Just diceNumber <- maybeRead (unpack text) :: Maybe Int,
+              diceNumber <= 20 && diceNumber > 0 = pure diceNumber
+
+        sanitizeNumberOfDice _ = invalidMessage diceMessage sanitizeNumberOfDice
 
     getSourceFile :: IO InputFile
     getSourceFile = fileSelectorPrompt >>= sanitizeGetSourceFile >>= readFile
 
-    sanitizeGetSourceFile :: Maybe [ Text ] -> IO FilePath
-    sanitizeGetSourceFile (Just (filePath:[])) = pure $ unpack filePath
-    sanitizeGetSourceFile _ = invalidMessage sourceMessage sanitizeGetSourceFile
+      where
+
+        sanitizeGetSourceFile :: Maybe [ Text ] -> IO FilePath
+        sanitizeGetSourceFile (Just (filePath:[])) = pure $ unpack filePath
+        sanitizeGetSourceFile _ = invalidMessage sourceMessage sanitizeGetSourceFile
 
     getOutputPath :: IO FilePath
     getOutputPath = outputFilePrompt >>= sanitizeGetOutputPath
 
-    sanitizeGetOutputPath :: Maybe Text -> IO FilePath
-    sanitizeGetOutputPath (Just filePath) = pure $ unpack filePath
-    sanitizeGetOutputPath _ = invalidMessage outputPathMessage sanitizeGetOutputPath
+      where
 
-    process :: ReaderT Stores IO ()
-    process = do
+        sanitizeGetOutputPath :: Maybe Text -> IO FilePath
+        sanitizeGetOutputPath (Just filePath) = pure $ unpack filePath
+        sanitizeGetOutputPath _ = invalidMessage outputPathMessage sanitizeGetOutputPath
 
-        ( numberOfDice , outputFile , outputPath ) <- asks processInner
-        lift $ writeFile outputPath outputFile
+process :: ReaderT Stores IO ()
+process = do
 
-        where 
+    ( numberOfDice , outputFile , outputPath ) <- asks processInner
+    lift $ writeFile outputPath outputFile
+
+  where 
             
-        processInner ( numberOfDice , inputFile , outputPath ) =
+    processInner ( numberOfDice , inputFile , outputPath ) =
             
-            ( numberOfDice , unwords . appendNumbers numberOfDice . lines $ inputFile ,
-            outputPath )
+      ( numberOfDice , unwords . appendNumbers numberOfDice . lines $ inputFile ,
+      outputPath )
 
     appendNumbers :: NumberOfDice -> [ Text ] -> [ Text ] 
     appendNumbers numberOfDice inputFile =
@@ -142,7 +161,7 @@ dicewarist = runReaderT process =<< inputs
         zipWith (putBetweenSemigroups ":") inputFile $
         ( iterate' ( addAnotherDice <*> ) [empty] ) !! numberOfDice
 
-        where
+      where
 
-            addAnotherDice :: [ Text -> Text ]
-            addAnotherDice = cons <$> [ '1' .. '6' ]
+        addAnotherDice :: [ Text -> Text ]
+        addAnotherDice = cons <$> [ '1' .. '6' ]
