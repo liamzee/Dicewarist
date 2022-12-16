38407c5ce80892f2791e36af5f547c5e0822ab5d
William Zhu <Liam.Zhu@protonmail.com>
2022-12-16 05:20:57 -0500

    Cleaned up style somewhat, removed unnecessary unsafePerformIO, used code formatter. Added strict Stores type instead of tuple.

--- app/Dicewarist.hs
+++ app/Dicewarist.hs
@@ -1,167 +1,166 @@
-{-# LANGUAGE LambdaCase, OverloadedStrings, NamedFieldPuns, RecordWildCards #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Dicewarist where
 
-import Data.List ( iterate' )
-import Data.Text
-    ( Text , cons , filter , lines , unwords , empty , unpack )
-import Data.Text.IO ( readFile , writeFile , putStrLn )
-import Prelude hiding ( filter , writeFile , unwords , readFile , lines )
-import Data.Maybe ( fromMaybe )
-import Graphics.UI.TinyFileDialogs
-    ( inputBox ,
-      messageBox ,
-      notifyPopup ,
-      openFileDialog ,
-      saveFileDialog ,
-      IconType ( Info , Error ) ,
-      OK ( OK ) )
-import GHC.Plugins ( HasCallStack )
-import GHC.IO ( unsafePerformIO )
-import GHC.Settings.Utils ( maybeRead )
+import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.Reader (ReaderT (runReaderT), asks)
-import Control.Monad.Trans (lift)
-import Data.Functor ((<&>))
+import Data.List (iterate')
+import Data.Text
+  ( Text,
+    cons,
+    empty,
+    filter,
+    lines,
+    unpack,
+    unwords,
+  )
+import Data.Text.IO (putStrLn, readFile, writeFile)
+import GHC.Settings.Utils (maybeRead)
+import Graphics.UI.TinyFileDialogs
+  ( IconType (Error, Info),
+    OK (OK),
+    inputBox,
+    messageBox,
+    notifyPopup,
+    openFileDialog,
+    saveFileDialog,
+  )
+import Prelude hiding (filter, lines, readFile, unwords, writeFile)
+
+-- Some general type synonyms.
 
 type NumberOfDice = Int
+
 type InputFile = Text
+
 type OutputPath = FilePath
-type Stores = ( NumberOfDice , InputFile , OutputPath )
 
-data InvalidMessage a =
-    
-    IM
-
-    { messageTitle :: Text
-    , messageContent :: Text
-    , errorMessage :: Text
-    , responseFunction :: IO ( Maybe a )
-    }
+data Stores = MkStore !NumberOfDice !InputFile !OutputPath
 
 -- names using InvalidMessage to pass data to functions, heavy in Texts
+-- InvalidMessage is a type used specifically to package data.
+
+data InvalidMessage a = IM
+  { messageTitle :: Text,
+    messageContent :: Text,
+    errorMessage :: Text,
+    responseFunction :: IO (Maybe a)
+  }
 
 diceMessage :: InvalidMessage Text
-diceMessage = 
-    
-    IM "Dice Selection Error" "You did not input a valid entry. Choose up to 20, or \
-    \press cancel to abort." "Aborted on dice selection." numberOfDicePrompt
+diceMessage =
+  IM
+    "Dice Selection Error"
+    "You did not input a valid entry. Choose up to 20, or \
+    \press cancel to abort."
+    "Aborted on dice selection."
+    numberOfDicePrompt
 
 sourceMessage :: InvalidMessage [Text]
-sourceMessage = 
-    
-    IM "Error on Selecting Source File" "You did not select a file. Try again, or \
-    \hit cancel to abort." "Aborted on source file selection." fileSelectorPrompt
+sourceMessage =
+  IM
+    "Error on Selecting Source File"
+    "You did not select a file. Try again, or \
+    \hit cancel to abort."
+    "Aborted on source file selection."
+    fileSelectorPrompt
 
 outputPathMessage :: InvalidMessage Text
-outputPathMessage = 
-    
-    IM "Error on Selecting Output File" "You did not select or create a file. Try \
-    \again, or hit cancel to abort." "Aborted on output file selection." outputFilePrompt
+outputPathMessage =
+  IM
+    "Error on Selecting Output File"
+    "You did not select or create a file. Try \
+    \again, or hit cancel to abort."
+    "Aborted on output file selection."
+    outputFilePrompt
 
 -- Common or reusable functions.
 
-putBetweenSemigroups :: Semigroup a => a -> a -> a -> a
-putBetweenSemigroups itemInBetween = ( <> ) . ( <> itemInBetween )
-
-
-{-# NOINLINE errorMessageBox #-}
-errorMessageBox :: forall b. HasCallStack => Text -> b
+errorMessageBox :: Text -> IO a
 errorMessageBox message =
-
-    unsafePerformIO $ notifyPopup "Program Aborted" message Error >>=
-    (error ( unpack message ) )
+  notifyPopup "Program Aborted" message Error
+    >>= error (unpack message)
 
 -- Functions closely tied up with message prompts, mainly the prompts and
 -- some basic error-handling code.
 
-
-
-removeCarriageReturns ::  Text -> Text 
-removeCarriageReturns = filter ( \u -> u /= '\n' && u /= '\r' )
-
-numberOfDicePrompt :: IO ( Maybe Text )
-numberOfDicePrompt = 
-
-    ((removeCarriageReturns <$>) <$>) $ inputBox "Number of Dice?" "How many dice would you like in \
-    \your diceware annotations? Choose at least 1, up to 20." $ Just "Input your number here."
-
+numberOfDicePrompt :: IO (Maybe Text)
+numberOfDicePrompt =
+  (fmap . fmap) removeCarriageReturns
+    $ inputBox
+      "Number of Dice?"
+      "How many dice would you like in \
+      \your diceware annotations? Choose between 1 and 20."
+    $ Just "Input your number here."
   where
-    
+    removeCarriageReturns :: Text -> Text
+    removeCarriageReturns = filter (`notElem` ("\n\r" :: String))
 
-fileSelectorPrompt :: IO ( Maybe [ Text ] )
-fileSelectorPrompt = openFileDialog "Select a File to Read" "" ["*.txt"] "" False
+fileSelectorPrompt :: IO (Maybe [Text])
+fileSelectorPrompt =
+  openFileDialog "Select a File to Read" "" ["*.txt"] "" False
 
-outputFilePrompt :: IO ( Maybe Text )
-outputFilePrompt = saveFileDialog "Select or Create a File to Output to" "" ["*.txt"] ""
+outputFilePrompt :: IO (Maybe Text)
+outputFilePrompt =
+  saveFileDialog "Select or Create a File to Output to" "" ["*.txt"] ""
 
-invalidMessage :: InvalidMessage a -> ( Maybe a -> IO b ) -> IO b
-invalidMessage IM { .. } callback =
-
-    messageBox messageTitle messageContent Info OK >>
-    responseFunction >>= callback
-    <$> pure . fromMaybe (errorMessageBox errorMessage)
+invalidMessage :: InvalidMessage a -> (Maybe a -> IO b) -> IO b
+invalidMessage IM {..} callback = do
+  messageBox messageTitle messageContent Info OK
+  responseFunction >>= \case
+    Nothing -> errorMessageBox errorMessage
+    u -> callback u
 
 -- Main program.
 
 dicewarist :: IO ()
-dicewarist = runReaderT process =<< inputs
+dicewarist = inputs >>= runReaderT process
 
 inputs :: IO Stores
-inputs = ((,,) <$> getNumberOfDice <*> getSourceFile <*> getOutputPath)
-
+inputs = MkStore <$> getNumberOfDice <*> getSourceFile <*> getOutputPath
   where
-
     getNumberOfDice :: IO NumberOfDice
     getNumberOfDice = numberOfDicePrompt >>= sanitizeNumberOfDice
-         
-      where 
-
+      where
         sanitizeNumberOfDice :: Maybe Text -> IO NumberOfDice
-        sanitizeNumberOfDice (Just text) 
-
-            | Just diceNumber <- maybeRead (unpack text) :: Maybe Int,
-              diceNumber <= 20 && diceNumber > 0 = pure diceNumber
-
-        sanitizeNumberOfDice _ = invalidMessage diceMessage sanitizeNumberOfDice
+        sanitizeNumberOfDice (Just text)
+          | Just diceNumber <- maybeRead (unpack text) :: Maybe Int,
+            diceNumber <= 20 && diceNumber > 0 =
+              pure diceNumber
+        sanitizeNumberOfDice Nothing = invalidMessage diceMessage sanitizeNumberOfDice
 
     getSourceFile :: IO InputFile
     getSourceFile = fileSelectorPrompt >>= sanitizeGetSourceFile >>= readFile
-
       where
-
-        sanitizeGetSourceFile :: Maybe [ Text ] -> IO FilePath
-        sanitizeGetSourceFile (Just (filePath:[])) = pure $ unpack filePath
-        sanitizeGetSourceFile _ = invalidMessage sourceMessage sanitizeGetSourceFile
+        sanitizeGetSourceFile :: Maybe [Text] -> IO FilePath
+        sanitizeGetSourceFile (Just [filePath]) = pure $ unpack filePath
+        sanitizeGetSourceFile Nothing = invalidMessage sourceMessage sanitizeGetSourceFile
 
     getOutputPath :: IO FilePath
     getOutputPath = outputFilePrompt >>= sanitizeGetOutputPath
-
       where
-
         sanitizeGetOutputPath :: Maybe Text -> IO FilePath
         sanitizeGetOutputPath (Just filePath) = pure $ unpack filePath
-        sanitizeGetOutputPath _ = invalidMessage outputPathMessage sanitizeGetOutputPath
+        sanitizeGetOutputPath Nothing = invalidMessage outputPathMessage sanitizeGetOutputPath
 
 process :: ReaderT Stores IO ()
 process = do
+  MkStore _ outputFile outputPath <- asks processInner
+  lift $ writeFile outputPath outputFile
+  where
+    processInner :: Stores -> Stores
+    processInner (MkStore numberOfDice inputFile outputPath) =
+      MkStore
+        numberOfDice
+        (unwords . appendNumbers numberOfDice . lines $ inputFile)
+        outputPath
 
-    ( numberOfDice , outputFile , outputPath ) <- asks processInner
-    lift $ writeFile outputPath outputFile
-
-  where 
-            
-    processInner ( numberOfDice , inputFile , outputPath ) =
-            
-      ( numberOfDice , unwords . appendNumbers numberOfDice . lines $ inputFile ,
-      outputPath )
-
-    appendNumbers :: NumberOfDice -> [ Text ] -> [ Text ] 
+    appendNumbers :: NumberOfDice -> [Text] -> [Text]
     appendNumbers numberOfDice inputFile =
-
-        zipWith (putBetweenSemigroups ":") inputFile $
-        ( iterate' ( addAnotherDice <*> ) [empty] ) !! numberOfDice
-
+      zipWith (\a b -> a <> ":" <> b) inputFile $
+        iterate' (addAnotherDice <*>) [empty] !! numberOfDice
       where
-
-        addAnotherDice :: [ Text -> Text ]
-        addAnotherDice = cons <$> [ '1' .. '6' ]
+        addAnotherDice :: [Text -> Text]
+        addAnotherDice = cons <$> ['1' .. '6']
--- app/Main.hs
+++ app/Main.hs
@@ -2,17 +2,26 @@
 
 module Main where
 
-import Dicewarist ( dicewarist )
+import Dicewarist (dicewarist)
 import Graphics.UI.TinyFileDialogs
+  ( IconType (Info),
+    OK (..),
+    messageBox,
+  )
 
 main :: IO ()
 main = do
-    initialize
-    dicewarist
-    pure ()
+  initialize
+  dicewarist
+  pure ()
 
 initialize :: IO OK
-initialize = messageBox "Dicewarist" "Welcome to Dicewarist. This application, to a \
+initialize =
+  messageBox
+    "Dicewarist"
+    "Welcome to Dicewarist. This application, to a \
     \wordlist specified purely with one word per line, appends Diceware numbers for a \
     \selected number of dice. The application will ask you to select a pre-formatted \
-    \file, input a number of dice, and select a file to output to." Info OK
+    \file, input a number of dice, and select a file to output to."
+    Info
+    OK
